45ee98eb94122c7eef8403ee8e8c3550b04204e5
Dileep T Paul <dileeptpaul2019@gmail.com>
2019-07-21 21:48:45 +0530

    first commit

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void firstTest(){
+		Assert.assertEquals("welcome", "welcome");
+	}
+}
